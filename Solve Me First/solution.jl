d7ffafc590572193e11471a5a63811ffeb83a8ce
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-10-30 20:40:23 +0530

    Create solution.jl
    https://www.hackerrank.com/challenges/solve-me-first

---
+++ Solve Me First/solution.jl
@@ -0,0 +1,12 @@
+function solve_me_first(x, y)
+	x+y
+end
+
+# Enter your code here 
+x = parse(Int, readline(stdin))
+y = parse(Int, readline(stdin))
+println(solve_me_first(x, y))
+
+
+//Solve Me First
+//https://www.hackerrank.com/challenges/solve-me-first
